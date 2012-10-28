b81b2d6dccdcbb4f520763a73047c9f71bccec0f
Wouter Cloete <githubDai6vddr@>
2012-10-28 14:57:01 +0200

    Auto build commit

--- Debug/cmn/CFIFO.d
+++
@@ -1,6 +0,0 @@
-cmn/CFIFO.d cmn/CFIFO.o: ../cmn/CFIFO.cpp ../cmn/CFIFO.h \
-  ../cmn/avrlibtypes.h
-
-../cmn/CFIFO.h:
-
-../cmn/avrlibtypes.h:
--- Debug/cmn/crc.d
+++
@@ -1,5 +0,0 @@
-cmn/crc.d cmn/crc.o: ../cmn/crc.cpp ../cmn/crc.h ../cmn/avrlibtypes.h
-
-../cmn/crc.h:
-
-../cmn/avrlibtypes.h:
