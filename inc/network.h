215b15c1aa2ef875eb2d3e1addfe3b6c03c95952
Wouter Cloete <wouter.cloete@firmlogik.co.za>
2012-10-31 16:12:17 +0200

    16:12

---
+++ inc/network.h
@@ -0,0 +1,86 @@
+#ifndef CNETWORK_H_
+#define CNETWORK_H_
+/****************************************************************************************/
+#include "avrlibtypes.h"
+#include "nodeid.h"
+#include "serial.h"
+/****************************************************************************************/
+namespace Cnetwork {
+  typedef enum {
+    T_BOOL = 0, T_CHAR = 1, T_UCHAR = 2, T_STRING = 11
+  } eDataTyep;
+  /****************************************************************************************/
+  typedef enum {
+    U2S_STATUSUPDATE = 1, U2S_OPEN_REQUEST,
+  } eMsgID;
+  /****************************************************************************************/
+  typedef enum {
+    E_UNIT_ID = 0, E_UNIT_GPS = 1, E_COVER_RFID = 2, E_COVER_STATUS = 3
+  } eMsgType;
+  /****************************************************************************************/
+  typedef enum {
+    COVER_CLOSED = 0, COVER_OPEN = 1,
+  } eCoverStatus;
+  /****************************************************************************************/
+  typedef struct {
+      u32 Length;
+      u08 MsgID;
+  } sHeader;
+  /****************************************************************************************/
+  typedef struct {
+      u08 elementID;
+      u08 type;
+      u08 strLen;
+  } sDataStringHeader;
+  /****************************************************************************************/
+  typedef struct {
+      u08 elementID;
+      u08 type;
+  } sDataBinHeader;
+  /****************************************************************************************/
+  typedef enum {
+    STATE_RX_LENGTH, STATE_RX_MSG_ID, STATE_RX_PAYLOAD, STATE_PACKET_AVAILABLE
+  } eState;
+  /****************************************************************************************/
+  class CNetwork {
+    private:
+      static const u16 MAX_PACKET_LEN = 256;
+      Cserial* serial;
+      u08 cntByte;
+      u32 timeLimit;
+      u16 baudRate;
+      u08 payloadSize;
+    public:
+      eState State;
+      u08 NodeId;
+      u08 healthy;
+      volatile u32 time;
+      sHeader Header;
+      u08* payload;
+      CNetwork(Cserial* serial, u08 size);
+      CNetwork(Cserial* serial, u08 size, u08 node);
+      void statusUpdate(u08* strID, u08* strGPS, u08* strRFID, eCoverStatus* status);
+      u08 setPayloadBufSize(u08 size);
+      void service(void);
+      u08 packetAvailable(void);
+      void reset(void);
+      void tx(u08 transactNum, u08 dstNode, u08* dat, u08 byteCnt);
+      u08 nodeidGet(void);
+      void nodeidSet(u08 ID);
+      u16 htons(u16* val) {
+        //u16 tmp;
+        *val = ((*val << 8) | (*val >> 8));
+        return *val;
+        //*val = tmp;
+      }
+      u32 htonl(u32* val) {
+        u16 tmpL, tmpR;
+        tmpL = *val >> 16;
+        tmpR = *val & 0x0000FFFF;
+        *val = (htons(&tmpL) | (u32) htons(&tmpR) << 16);
+        return *val;
+      }
+  };
+/****************************************************************************************/
+}
+#endif
---
+++ src/network.cpp
@@ -0,0 +1,163 @@
+/****************************************************************************************/
+#include <stdlib.h>
+#include <string.h>
+#ifndef WIN32
+#include <avr/sleep.h>
+#include <avr/eeprom.h>
+#include <util/atomic.h>
+#include "iopins.h"
+#else
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ComCtrls.hpp>
+#include <Menus.hpp>
+#include <ExtCtrls.hpp>
+#include "main.h"
+#define ATOMIC_BLOCK(val)
+#endif
+/****************************************************************************************/
+#include <CUART.h>
+#include "crc.h"
+#include "avrlibtypes.h"
+#include "network.h"
+/****************************************************************************************/
+using namespace Cnetwork;
+/****************************************************************************************/
+#undef DEBUG
+/****************************************************************************************/
+#ifndef WIN32
+#define EE_NODEID			(u08 *)(0x40)
+#else
+AnsiString str;
+#endif
+/****************************************************************************************/
+CNetwork::CNetwork(Cserial* serial, u08 size) {
+  this->serial = serial;
+  State = STATE_RX_LENGTH;
+  time = 0;
+  timeLimit = 0;
+  payloadSize = 0;
+  payload = 0;
+  healthy = true;
+  baudRate = this->serial->getBaudRate();
+  setPayloadBufSize(size);
+}
+/****************************************************************************************/
+CNetwork::CNetwork(Cserial* serial, u08 size, u08 node) {
+  this->serial = serial;
+  this->NodeId = node;
+  State = STATE_RX_LENGTH;
+  time = 0;
+  timeLimit = 0;
+  payloadSize = 0;
+  payload = 0;
+  healthy = true;
+  baudRate = this->serial->getBaudRate();
+  setPayloadBufSize(size);
+}
+/****************************************************************************************/
+u08 CNetwork::setPayloadBufSize(u08 size) {
+  payload = (u08*) malloc(size);
+  if (payload == NULL) {
+    while (1) {
+      payload = NULL;
+    }
+  }
+  payloadSize = size;
+  return true;
+}
+/****************************************************************************************/
+void CNetwork::service(void) {
+  serial->service();
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+  {
+    if (time > timeLimit) {
+      State = STATE_RX_LENGTH;
+      time = 0;
+    }
+  }
+  switch (State) {
+    case STATE_RX_LENGTH:
+      if (serial->rxnum() > 4) {
+        serial->receive((u08*) &Header.Length, 4);
+        htonl(&Header.Length);
+        if (Header.Length <= MAX_PACKET_LEN) {
+          State = STATE_RX_MSG_ID;
+        }
+      }
+      break;
+    case STATE_RX_MSG_ID:
+      if (serial->receive(&Header.MsgID, 1) == 1) {
+        cntByte = 1;
+        State = STATE_RX_PAYLOAD;
+      }
+      break;
+    case STATE_RX_PAYLOAD:
+      if (serial->receive(&payload[cntByte], 1) == 1) {
+        cntByte++;
+        if (cntByte == Header.Length) {
+          State = STATE_PACKET_AVAILABLE;
+        }
+      }
+      break;
+    case STATE_PACKET_AVAILABLE:
+      ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+      {
+        time = 0;
+      }
+      break;
+    default:
+      State = STATE_RX_LENGTH;
+      break;
+  }
+}
+/****************************************************************************************/
+void CNetwork::reset(void) {
+  State = STATE_RX_LENGTH;
+}
+/****************************************************************************************/
+u08 CNetwork::packetAvailable(void) {
+  if (State == STATE_PACKET_AVAILABLE) {
+    return true;
+  }
+  return false;
+}
+/****************************************************************************************/
+void CNetwork::statusUpdate(u08* strID, u08* strGPS, u08* strRFID, eCoverStatus* status) {
+  sHeader Header;
+  sDataStringHeader strHdr;
+  sDataBinHeader binHdr;
+
+  reset();
+  serial->clearTx();
+  serial->clearRx();
+  Header.Length = 13 + strlen((c08*) strID) + strlen((c08*) strGPS)
+      + strlen((c08*) strRFID);
+  Header.MsgID = U2S_STATUSUPDATE;
+  serial->send((u08*) &Header, sizeof(Header));
+  // E_UNIT_ID
+  strHdr.elementID = E_UNIT_ID;
+  strHdr.type = T_STRING;
+  strHdr.strLen = strlen((c08*) strID);
+  serial->send((u08*) &strHdr, sizeof(sDataStringHeader));
+  serial->sendStr((c08*) strID);
+  // E_UNIT_GPS
+  strHdr.elementID = E_UNIT_GPS;
+  strHdr.type = T_STRING;
+  strHdr.strLen = strlen((c08*) strGPS);
+  serial->send((u08*) &strHdr, sizeof(sDataStringHeader));
+  serial->sendStr((c08*) strGPS);
+  // E_COVER_RFID
+  strHdr.elementID = E_COVER_RFID;
+  strHdr.type = T_STRING;
+  strHdr.strLen = strlen((c08*) strRFID);
+  serial->send((u08*) &strHdr, sizeof(sDataStringHeader));
+  serial->sendStr((c08*) strRFID);
+  // E_COVER_STATUS
+  binHdr.elementID = E_COVER_RFID;
+  binHdr.type = T_UCHAR;
+  serial->send((u08*) &binHdr, sizeof(sDataBinHeader));
+  serial->send((u08*) status, 1);
+}
