ee00629ba94ad22a669417b0f2e5c360a4512af3
Wouter Cloete <wouter.cloete@firmlogik.co.za>
2012-11-05 01:32:57 +0200

    Iets wat werk !!

--- main.cpp
+++ main.cpp
@@ -67,15 +67,15 @@ int main(void) {
     goto retry;
   display.writeStringP(PSTR("Done    "), 0, 3, false);
 
-  status = COVER_OPEN;
-  while (1) {
-    server.sendCoverStatus(status);
-    if (status == COVER_OPEN)
-      status = COVER_CLOSED;
-    else
-      status = COVER_OPEN;
-    _delay_ms(20);
-  }
+//  status = COVER_OPEN;
+//  while (1) {
+//    server.sendCoverStatus(status);
+//    if (status == COVER_OPEN)
+//      status = COVER_CLOSED;
+//    else
+//      status = COVER_OPEN;
+//    _delay_ms(20);
+//  }
   while (1) {
     _delay_ms(500);
     display.writeStringP(PSTR("Manhole Lock System"), 0, 0);
